c9626c4f547ac708bfcdea488e227bf89011b62d
Yaroslav <59771733+keyoneinc@users.noreply.github.com>
2024-07-11 19:56:28 +0100

    added Forum class and it's defenition

--- cw39_task3/Forum.cpp
+++ cw39_task3/Forum.cpp
@@ -2,20 +2,68 @@
 
 void Forum::signUp(string login, string pass)
 {
+	if (Users.find(login) == Users.end()) {
+		Users[login] = pass;
+		cout << "New user added successfuly";
+	}
+	else {
+		cout << "Error. This user already exists.";
+	}
 }
 
 void Forum::signIn(string login, string pass)
 {
+	if (Users.find(login) == Users.end()) {
+		cout << "Error. This does not exist.";
+	}
+	else {
+		if (find(signInUsers.begin(), signInUsers.end(), login) != signInUsers.end()) {
+			cout << "You are already logged in.";
+			return;
+		}
+		if (Users[login] == pass) {
+			cout << "You've logged in.";
+			signInUsers.push_back(login);
+		}
+		else {
+			cout << "The password is incorrect. Try again";
+		}
+	}
 }
 
 void Forum::signOut(string login)
 {
+	if (Users.find(login) == Users.end()) {
+		cout << "Error. This does not exist.";
+	}
+	else {
+		if (find(signInUsers.begin(), signInUsers.end(), login) != signInUsers.end()) {
+			auto it = find(signInUsers.begin(), signInUsers.end(), login);
+			signInUsers.erase(it);
+			cout << "You've logged out.";
+			
+		}
+		else {
+			cout << "You are not logged in.";
+			return;
+		}
+
+
+	}
 }
 
 void Forum::showAllUsers() const
 {
+	for (auto item : Users) {
+		cout << "Login: " << item.first << endl;
+		cout << "Password: " << item.second << endl << endl;
+	}
 }
 
 void Forum::showSignedInUsers() const
 {
+	cout << "Signed in users: " << endl;
+	for (auto login : signInUsers) {
+		cout << "Login: " << login << endl;
+	}
 }
